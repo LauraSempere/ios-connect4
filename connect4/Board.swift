f6b1ebebc673c6b966d8756d0971416df4b2f29d
Laura Sempere <lrsempere@gmail.com>
2017-05-26 17:13:20 +0800

    Show Chip animated when user taps on a column of the game board + Board model class

---
+++ connect4/Board.swift
@@ -0,0 +1,57 @@
+//
+//  Board.swift
+//  connect4
+//
+//  Created by Laura Scully on 25/5/2017.
+//  Copyright © 2017 laura.sempere.com. All rights reserved.
+//
+
+import Foundation
+
+enum ChipColor: Int {
+    case none = 0
+    case red
+    case yellow
+}
+
+class Board: NSObject {
+    
+    // Number of spots per width and height
+    static var width = 7
+    static var height = 6
+    
+    var spots = [ChipColor]()
+    var activeChip:ChipColor = .red
+    
+    override init() {
+        // Initialize the Board with empty spaces
+        for _ in 0 ..< Board.width * Board.height {
+            self.spots.append(.none)
+        }
+        super.init()
+    }
+    
+    func swapTurn() {
+        if activeChip == .red {
+            activeChip = .yellow
+        } else {
+            activeChip = .red
+        }
+    }
+    
+    func nextEmptyRow(at column: Int) -> Int? {
+        for row in 0 ..< Board.height {
+            let currChip = spots[row + column * Board.height]
+            if currChip == .none {
+                return row
+            }
+        }
+        return nil
+    }
+    
+    func add(chip: ChipColor, column:Int) {
+        if let row = nextEmptyRow(at: column) {
+            spots[row + column * Board.height] = chip
+        }
+    }
+}
--- connect4/GameViewController.swift
+++ connect4/GameViewController.swift
@@ -10,17 +10,62 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    @IBOutlet var columnButtons: [UIButton]!
     @IBOutlet weak var gameBoard: UIStackView!
-
+    @IBOutlet var columnViews: [UIView]!
+    
+    var board = Board()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    private func imageFor(chipColor: ChipColor) -> UIImage? {
+        switch chipColor {
+        case .red: return UIImage(named: "redChip")!
+        case .yellow: return UIImage(named: "yellowChip")!
+        default: return nil
+        }
     }
     
 
     @IBAction func columnButtonDidTap(_ sender: UIButton) {
         print("Button in Column Tapped! \(sender.tag)")
+        if let row =  board.nextEmptyRow(at: sender.tag) {
+            board.add(chip: board.activeChip, column: sender.tag)
+            displayChip(imageFor(chipColor: board.activeChip)!, at: sender.tag, row: row)
+            
+            board.swapTurn()
+        }
+    }
+    
+    func displayChip(_ chipImage: UIImage, at column:Int, row: Int) {
+        
+        let offSet:CGFloat = 2
+        let columnView = columnViews[column]
+        let chipSize = max(columnView.frame.width - offSet, columnView.frame.height / 6)
+        let chipFrame = CGRect(x: 0, y: 0, width: chipSize, height: chipSize)
+        
+        let chip = UIImageView()
+        chip.image = chipImage
+        chip.frame = chipFrame
+        chip.contentMode = .scaleAspectFit
+
+        let x = columnView.frame.midX + gameBoard.frame.minX
+        var y = columnView.frame.maxY - chipSize / 2 + gameBoard.frame.minY
+        y -= chipSize * CGFloat(row)
+        
+        chip.center = CGPoint(x: x, y: y)
+        
+        // Initial position of the chip out of the view
+        chip.transform = CGAffineTransform(translationX: 0, y: -800)
+        
+        view.addSubview(chip)
+        
+        // Chip Animation in
+        UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseIn, animations: {
+          chip.transform = CGAffineTransform.identity
+        })
+        
     }
 
 
